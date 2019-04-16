631eb6fb5aad3e24acd11e71c67ae53c43241c3e
Denis Papathanasiou <denis@papathanasiou.org>
2019-04-16 18:40:25 -0400

    Implemented the pascal's triangle problem

--- epi_judge_python/pascal_triangle.py
+++ epi_judge_python/pascal_triangle.py
@@ -2,8 +2,16 @@ from test_framework import generic_test
 
 
 def generate_pascal_triangle(n):
-    # TODO - you fill in here.
-    return []
+    triangle = []
+    for i in range(n):
+        row = []
+        for j in range(i+1):
+            if j == 0 or j == i:
+                row.append(1)
+            else:
+                row.append((triangle[i-1][j-1] + triangle[i-1][j]))
+        triangle.append(row)
+    return triangle
 
 
 if __name__ == '__main__':
