92332b65265b7896e2d2f1125179c63ab456e96a
Denis Papathanasiou <denis@papathanasiou.org>
2019-04-16 19:23:18 -0400

    Implemented the fibonacci sequence with dynamic programming

--- epi_judge_python/fibonacci.py
+++ epi_judge_python/fibonacci.py
@@ -2,8 +2,17 @@ from test_framework import generic_test
 
 
 def fibonacci(n):
-    # TODO - you fill in here.
-    return -1
+    if n == 0:
+        return 0
+
+    r = [0] * (n+1)
+    r[0] = 0
+    r[1] = 1
+    if n > 1:
+        for i in range(2, n+1):
+            r[i] = r[i-1] + r[i-2]
+            
+    return r[n]
 
 
 if __name__ == '__main__':
