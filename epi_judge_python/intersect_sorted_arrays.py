e1769e5124a03aee965385b056cc29c9f60fbeb7
Denis Papathanasiou <denis@papathanasiou.org>
2019-04-17 20:50:17 -0400

    Intersection of two sorted arrays, with one more optimization

--- epi_judge_python/intersect_sorted_arrays.py
+++ epi_judge_python/intersect_sorted_arrays.py
@@ -20,8 +20,8 @@ def intersect_two_sorted_arrays(A, B):
     #return sorted(set(A) & set(B))
 
     if len(A) < len(B):
-        return sorted(set([x for x in A if binary_search(B, x)]))
-    return sorted(set([x for x in B if binary_search(A, x)]))
+        return sorted(set([x for x in list(set(A)) if binary_search(B, x)]))
+    return sorted(set([x for x in list(set(B)) if binary_search(A, x)]))
 
 
 if __name__ == '__main__':
