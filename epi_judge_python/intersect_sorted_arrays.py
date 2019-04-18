b2d2089fdb81db9781e36abc7e1d779efa2efee3
Denis Papathanasiou <denis@papathanasiou.org>
2019-04-17 20:47:49 -0400

    Intersection of two sorted array simple, this time with a home-brewed binary search

--- epi_judge_python/intersect_sorted_arrays.py
+++ epi_judge_python/intersect_sorted_arrays.py
@@ -1,8 +1,27 @@
 from test_framework import generic_test
 
 
+def binary_search(lst, elem):
+    """Is elem in the list?"""
+
+    l = len(lst)
+    if l == 0:
+        return False
+
+    m = l // 2
+    if lst[m] == elem:
+        return True
+    elif elem < lst[m]:
+        return binary_search(lst[:m], elem)
+    else:
+        return binary_search(lst[m+1:], elem)
+
 def intersect_two_sorted_arrays(A, B):
-    return sorted(set(A) & set(B))
+    #return sorted(set(A) & set(B))
+
+    if len(A) < len(B):
+        return sorted(set([x for x in A if binary_search(B, x)]))
+    return sorted(set([x for x in B if binary_search(A, x)]))
 
 
 if __name__ == '__main__':
