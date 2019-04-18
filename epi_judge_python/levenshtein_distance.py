caa5394437781d6be04dff07e7ca8f43550ed2e0
Denis Papathanasiou <denis@papathanasiou.org>
2019-04-18 19:12:15 -0400

    Implemented the Levenshtein edit distance as a dp problem

--- epi_judge_python/levenshtein_distance.py
+++ epi_judge_python/levenshtein_distance.py
@@ -2,8 +2,18 @@ from test_framework import generic_test
 
 
 def levenshtein_distance(A, B):
-    # TODO - you fill in here.
-    return 0
+    """Minimize 3 possible actions: edit, delete, insert"""
+
+    g = [[i+j for j in range(len(B)+1)] for i in range(len(A)+1)]
+
+    for i in range(1, len(A)+1):
+        for j in range(1, len(B)+1):
+            if A[i-1] == B[j-1]:
+                g[i][j] = g[i-1][j-1]
+            else:
+                g[i][j] = 1 + min(g[i-1][j-1], min(g[i-1][j], g[i][j-1]))
+                
+    return g[-1][-1]
 
 
 if __name__ == '__main__':
