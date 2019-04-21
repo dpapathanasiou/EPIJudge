623c76ae0cf4fad26efdf182c75a00d90e262df2
Denis Papathanasiou <denis@papathanasiou.org>
2019-04-21 17:55:25 -0400

    Solved anagrams by using hash tables (python dicts) versus sorted lists of words

--- epi_judge_python/anagrams.py
+++ epi_judge_python/anagrams.py
@@ -2,8 +2,22 @@ from test_framework import generic_test, test_utils
 
 
 def find_anagrams(dictionary):
-    # TODO - you fill in here.
-    return []
+    d = {} # k = sorted word, v = [word(s)]
+    h = {} # k = word, v = sorted string
+    for word in dictionary:
+        w = ''.join(sorted(word))
+        h[word] = w
+        words = d.get(w, [])
+        words.append(word)
+        d[w] = words
+
+    anagrams = []
+    for word in dictionary:
+        words = d[h[word]]
+        if len(words) > 1:
+            if words not in anagrams:
+                anagrams.append(words)
+    return anagrams
 
 
 if __name__ == '__main__':
