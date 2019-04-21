f31f9e8a194c1a848aaef6198c8433d8d064d989
Denis Papathanasiou <denis@papathanasiou.org>
2019-04-21 18:41:42 -0400

    Searching a sorted array for the first instance of a value: use binary search, but don't stop at the first match; keep going leftwards in case there's a prior one

--- epi_judge_python/search_first_key.py
+++ epi_judge_python/search_first_key.py
@@ -2,8 +2,20 @@ from test_framework import generic_test
 
 
 def search_first_of_k(A, k):
-    # TODO - you fill in here.
-    return 0
+    left  = 0
+    right = len(A) - 1
+    match = -1
+    while left <= right:
+        m = (left + right) // 2
+        if A[m] > k:
+            right = m - 1
+        elif A[m] == k:
+            # match found, but keep looking leftwards for the first one (if this is not it)
+            match = m
+            right = m - 1
+        else:
+            left = m + 1
+    return match
 
 
 if __name__ == '__main__':
