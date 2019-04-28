eb3c7d06beff6ac459ba0c0ac1762528ab811264
Denis Papathanasiou <denis@papathanasiou.org>
2019-04-28 19:52:35 -0400

    Implemented reverse polish notation calculator, with help of wikipedia: https://en.wikipedia.org/wiki/Reverse_Polish_notation

--- epi_judge_python/evaluate_rpn.py
+++ epi_judge_python/evaluate_rpn.py
@@ -1,10 +1,39 @@
 from test_framework import generic_test
 
+'''
+https://en.wikipedia.org/wiki/Reverse_Polish_notation
 
+The following algorithm evaluates postfix expressions using a stack, with the expression processed from left to right:
+
+for each token in the postfix expression:
+  if token is an operator:
+    operand_2 ← pop from the stack
+    operand_1 ← pop from the stack
+    result ← evaluate token with operand_1 and operand_2
+    push result back onto the stack
+  else if token is an operand:
+    push token onto the stack
+result ← pop from the stack
+
+'''
 def evaluate(expression):
-    # TODO - you fill in here.
-    return 0
+    results = []
+    for token in expression.split(','):
+        if token in ['+', '-', '*', '/']:
+            y = results.pop()
+            x = results.pop()
+            if token == '+':
+                results.append(x + y)
+            elif token == '-':
+                results.append(x - y)
+            elif token == '*':
+                results.append(x * y)
+            else:
+                results.append(int(x / y))
+        else:
+            results.append(int(token))
 
+    return results.pop()
 
 if __name__ == '__main__':
     exit(
